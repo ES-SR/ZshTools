5d7ee0e8191be42d12ae2f44e18140487fe6997b
michael kilpatrick <imincairo@users.noreply.github.com>
2025-06-29 17:49:51 +0700

    Update and rename @read:autoEOF.zsh to @read/autoEOF.zsh
    fixed some edge cases like when reading content containing :
    `gawk'
    which would cause unmatched quote error, as well as try to perform command substitution

---
+++ @read/autoEOF.zsh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+##  @read:autoEOF
+###
+
+
+() {
+
+function __@read:autoEOF {
+	local MAX_BLANK=${1:-2}
+	local TIMEOUT=${2:-1}
+	local DELAY=${3:-0}
+	local LINE CNT
+	while [[ ${${${LINE::="$(IFS= read -rse)"}:+$CNT}:-$((CNT++))} -lt ${MAX_BLANK} ]] {
+		if [[ -n $LINE ]] {
+			while [[ $CNT -gt 0 ]] {
+				echo ""
+				((CNT=CNT-1))
+			}
+		}
+
+		echo -n "${LINE}${LINE:+"\n"}"
+		sleep $DELAY
+	}
+}
+
+__@read:autoEOF "$@"
+
+} "$@"
