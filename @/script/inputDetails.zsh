8db8bacd4dc177a051bde4961babdd3cea0a91d0
michael kilpatrick <imincairo@users.noreply.github.com>
2025-09-21 03:03:29 +0700

    Update inputDetails.zsh

--- @/script/inputDetails.zsh
+++ @/script/inputDetails.zsh
@@ -1,29 +1,42 @@
 #!/usr/bin/env zsh
 ##  @script:inputDetails
-
+###   get information on a functions inputs
 
 () {
 
-  for A ( "$@" ) {
-    pint -n - "${A}: "
-    if [[ -p "${A}" ]] {
-      print "File Descriptor or Named Pipe"
-      continue
-    }
 
-		Type=${${(As.: .)="$(type -w ${(P)A} 2>/dev/null)"}[2]} \
-		|| Type=${${(As.: .)="$(type -w $A 2>/dev/null)"}[2]} \
-		|| Type=$(print ${(tP)A})
-	  if [[ -z $Type ]] {
-      Type=$(print ${(t)A})
+	autoload -Uz @script:inputDetails
+	function @script:inputDetails {
+		if [[ -p /dev/stdin ]] {
+			print "/dev/stdin: Pipe"
 		}
-		print -n ${Type}${Type:+"\n"}
-  }
-}
+
+		for A ( "$@" ) {
+			pint -n - "${A}: "
+			if [[ -p "${A}" ]] {
+				print "File Descriptor or Named Pipe"
+				continue
+			}
+
+			local Type
+			Type=${${(As.: .)="$(type -w ${(P)A} 2>/dev/null)"}[2]} \
+			|| Type=${${(As.: .)="$(type -w $A 2>/dev/null)"}[2]} \
+			|| Type=$(print ${(tP)A})
+			if [[ -z $Type ]] {
+				Type=$(print ${(t)A})
+			}
+			print -n ${Type}${Type:+"\n"}
+		}
+	}
+
+
+	@script:inputDetails "$@"
+} "$@"
 
 :<<-"Examples.@script:inputDetails"
 	% Arr=( {1..4} )
-	% @script:input:type <( echo "hello" )  "$Arr" Arr ${(A):-"${options[(I)*pushd*]}"} echo "no more args"
+	% echo "hello" | @script:input:type <( echo "hello" )  "$Arr" Arr ${(A):-"${options[(I)*pushd*]}"} echo "no more args"
+	/dev/stdin: Pipe
 	/proc/self/fd/12: FD
 	1 2 3 4: array-special
 	Arr: array
