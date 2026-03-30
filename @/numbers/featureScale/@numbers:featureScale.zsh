210d68028866a9b9e085d83b60ef09a70ae97da0
michael kilpatrick <imincairo@users.noreply.github.com>
2026-03-30 11:07:05 +0700

    Add featureScale function for normalizing numbers

---
+++ @/numbers/featureScale/@numbers:featureScale.zsh
@@ -0,0 +1,53 @@
+
+function @numbers:featureScale {
+	emulate -L zsh; setopt extendedglob typesetsilent
+
+	local -a Range Numbers Normalized Args
+	local Sort NumberStr
+	local -i SortIdx N 
+	local A=0.0 B=1.0 Min Max #no type so they can be cast based on input
+
+	Args=(${(@s.=.s. .)=argv})
+
+	declare -A SortOrder=(['+']="n" ['-']="On" ['[rR]']="Oa")
+	SortIdx=${Args[(I)(+|-)]}
+	(( SortIdx )) && {
+		Sort=$Args[$((SortIdx))]
+		Args[$SortIdx]=()
+	}
+	Sort=${${SortOrder[(k)$Sort]}:-"a"}
+
+	local -i RangeIdx=${argv[(I)(-|)([0-9.]##|)[,](-|)([0-9.]##|)]}
+	(( $RangeIdx )) && {
+		Range=(${(-s.,.)${(P)RangeIdx}})
+		Args[$RangeIdx]=()
+		A=${Range[1]}
+		B=${Range[2]}
+	}
+
+	Min=${${(-)Args}[1]}
+	Max=${${(-)Args}[-1]}
+
+	(( Max - Min )) || {
+		print -u2 -P -- "%K{red}%F{black}Error: All input numbers are identical. Cannot normalize.%f%k"
+	}
+
+	A=${A:-$Min}
+	B=${B:-$Max}
+	(( A - B )) || {
+		print -u2 -P -- %K{yellow}%F{black} range produces constant output %f%k
+		for N ( {1..$#} ) {
+			Normalized+=("${A}")
+		}
+		print -- $Normalized
+		return 1
+	}
+
+	Numbers=(${(e):-"${(${Sort})Args}"})
+	for N ( ${(a)Numbers} ) {
+		Normalized+=($(( A + ( (N-Min)*(B-A) / (Max-Min) ) )))
+	}
+	: <<< "TODO:add flag to output orginal and new values (output used while developing function)"
+	print -- $Normalized
+}
+
