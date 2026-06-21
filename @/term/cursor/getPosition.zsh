25eecbdc05e5a6ce2ec611ed9c2ae8b70da7e24b
michael kilpatrick <imincairo@users.noreply.github.com>
2026-06-22 03:08:30 +0700

    Refactor getPosition function and update examples
    Refactor the getPosition function to improve clarity and structure. Update examples for better understanding.

--- @/term/cursor/getPosition.zsh
+++ @/term/cursor/getPosition.zsh
@@ -1,51 +1,55 @@
-#!/bin/zsh
-
-
-() {
-
-autoload -Uz @term:cursor:getPosition
 function @term:cursor:getPosition {
-	tput u7 > $TTY
-	local CPos
-	IFS='[;' read -rsd R -A CPos
-	local -a CursorPosition=( ${CPos[2,3]} )
-	: ${(A)CLine::=${(j:,:)CursorPosition[1]}}
-	: ${(A)CCol::=${(j:,:)CursorPosition[2]}}
-	local -a Output=( ${${(s..)${@//-/}}// /} )
-	
-	(( $#Output )) && {
-		(( ${Output[(I)l]} )) && { Output[${Output[(i)l]}]=$CLine }
-		(( ${Output[(I)c]} )) && { Output[${Output[(i)c]}]=$CCol }
-		print ${(j.,.)Output}
-	}
+        emulate -L zsh
+
+        local -h OriginalSTTY
+        OriginalSTTY="$(stty -g)"
+{
+        stty raw -echo min 0 time 2
+
+        tput u7 > $TTY
+
+        local -a CPos
+        IFS='[;' read -rsd R -A CPos
+        CPos=( ${CPos[2,3]} )
+} always {
+        stty "${OriginalSTTY}" 2>/dev/null
+}
+        # because they are not declared before use and assigned with ::= CLine and CCol are made global
+        : ${(A)CLine::=${(j:,:)CPos[1]}}
+        : ${(A)CCol::=${(j:,:)CPos[2]}}
+        local -a Output=( ${${(s..)${@//-/}}// /} )
+
+        (( $#Output )) && {
+                (( ${Output[(I)l]} )) && { Output[${Output[(i)l]}]=$CLine }
+                (( ${Output[(I)c]} )) && { Output[${Output[(i)c]}]=$CCol }
+                print -- ${(j.,.)Output}
+        }
 }
 
-}
-
-:<<-"EXAMPLES"
-		** Line numbers are skewed from deleting my prompt lines when adding the examples **
-	~/ @term:cursor:getPosition
-	
-	~/ @term:cursor:getPosition l
-	21
-	~/ @term:cursor:getPosition c
-	1
-	~/ @term:cursor:getPosition cl
-	1,27
-	~/ @term:cursor:getPosition lc
-	30,1
-	~/ @term:cursor:getPosition -lc
-	33,1
-	~/ @term:cursor:getPosition -l -c
-	36,1
-	~/ @term:cursor:getPosition -l   
-	37
-	~/ clear; print -P - "%B%S${(%l:((COLUMNS/2))::-:: :r:((COLUMNS/2))::-:: :):-$(@term:cursor:getPosition l)}%s%b"
-	------------------------------------------------ 1,1 -----------------------------------------------
-	~/ print -P - "%B%S${(%l:((COLUMNS/2))::-:: :r:((COLUMNS/2))::-:: :):-$(@term:cursor:getPosition lc)}%s%b"       
-	------------------------------------------------ 5,1 -----------------------------------------------
-	~/ print -P - "%B%S${(%l:((COLUMNS/2))::-:: :r:((COLUMNS/2))::-:: :):-$(@term:cursor:getPosition l)}%s%b" 
-	------------------------------------------------- 9 ------------------------------------------------
-	~/ print -P - "%B%S${(%l:((COLUMNS/2))::-:: :r:((COLUMNS/2))::-:: :):-$(@term:cursor:getPosition l)}%s%b"
-	------------------------------------------------ 13 ------------------------------------------------
+<<-"EXAMPLES"
+        @term:cursor:getPosition
+        @term:cursor:getPosition l
+        @term:cursor:getPosition c
+        @term:cursor:getPosition cl
+        @term:cursor:getPosition lc
+        @term:cursor:getPosition -lc
+        @term:cursor:getPosition -l -c
+        @term:cursor:getPosition -l
+        print -P - "%B%S${(%l:((COLUMNS/2))::-:: :r:((COLUMNS/2))::-:: :):-$(@term:cursor:getPosition l)}%s%b"
+        print -P - "%B%S${(%l:((COLUMNS/2))::-:: :r:((COLUMNS/2))::-:: :):-$(@term:cursor:getPosition lc)}%s%b"
+        print -P - "%B%S${(%l:((COLUMNS/2))::-:: :r:((COLUMNS/2))::-:: :):-$(@term:cursor:getPosition l)}%s%b"
+        print -P - "%B%S${(%l:((COLUMNS/2))::-:: :r:((COLUMNS/2))::-:: :):-$(@term:cursor:getPosition l)}%s%b"
+        () {
+                @term:cursor:getPosition
+                print -n -- $CCol
+                tput cuf 10
+                @term:cursor:getPosition
+                print -n -- $CCol
+                tput cuf 10
+                @term:cursor:getPosition
+                print -n -- $CCol
+                tput cuf 10
+                @term:cursor:getPosition
+                print -- $CCol
+        }
 EXAMPLES
